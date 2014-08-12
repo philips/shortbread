acc8514c87624d610cdc404b81cd995d853f4ad3
Shantanu <joshishantanu4@gmail.com>
2014-08-12 12:21:17 -0700

    fingerprint incorporated, as well as revoke mechanism

--- server/server.go
+++ server/server.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"crypto/md5"
 	"crypto/rand"
+	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 
 	"code.google.com/p/go.crypto/ssh"
 )
 
-type CertificateCollection map[string][]*ssh.Certificate
+type CertificateCollection map[[16]byte][]*ssh.Certificate
 
 type CertificateParameters struct {
 	CertType   string
@@ -53,6 +57,14 @@ func (c CertificateCollection) New(params CertificateParameters) error {
 		panic("public key is nil")
 	}
 
+	// encoded := strings.Split(params.Key, " ")[1]
+	// data, err := base64.StdEncoding.DecodeString(encoded)
+	// if err != nil {
+	// 	fmt.Println("error:", err)
+	// 	return err
+	// }
+	// fmt.Printf("fingerPrint: % x", md5.Sum(data))
+
 	cert := &ssh.Certificate{
 		Nonce:       []byte{},
 		Key:         keyToSign,
@@ -79,12 +91,18 @@ func (c CertificateCollection) New(params CertificateParameters) error {
 		return err
 	}
 
-	// add newly created cert to the global map + write to local disk (for now).
-	certs, ok := c[params.User]
+	// add newly created cert to the global map (with fingerprint as key) and then write to local disk (for now).
+	fingerprint, err := getFingerPrint(params.Key)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Final check: % x", fingerprint)
+
+	certs, ok := c[fingerprint]
 	if !ok {
-		c[params.User] = []*ssh.Certificate{cert}
+		c[fingerprint] = []*ssh.Certificate{cert}
 	} else {
-		c[params.User] = append(certs, cert)
+		c[fingerprint] = append(certs, cert)
 	}
 
 	err = ioutil.WriteFile(os.ExpandEnv("$HOME/.ssh/id_rsa-cert.pub"), ssh.MarshalAuthorizedKey(cert), 0600)
@@ -95,14 +113,34 @@ func (c CertificateCollection) New(params CertificateParameters) error {
 	return nil
 }
 
+// Revoke uses the public key provided in the request to delete the corresponding certificate
+// from the map. However, if a username is provided then a certificate is deleted only if the fingerprint is found in the map
+// and the user is listed as a valid principal in the certificate. Reports an error otherwise.
 func (c CertificateCollection) Revoke(revokeInfo RevokeInfo) error {
-	key := revokeInfo.Key
-
-	if _, ok := c[key]; !ok {
-		return fmt.Errorf("user %s does not exist", revokeInfo.User)
+	fingerprint, err := getFingerPrint(revokeInfo.Key)
+	if err != nil {
+		return err
 	}
 
-	delete(c, key)
+	certs, ok := c[fingerprint]
+	if !ok {
+		return errors.New("certificate not found, check if you have specified the correct public key")
+	}
+
+	user := revokeInfo.User
+	checkPrincipal := func(certs []*ssh.Certificate, principal string) bool {
+		for _, cert := range certs {
+			if cert.ValidPrincipals[0] == principal {
+				return true
+			}
+		}
+		return false
+	}
+	if user != "" && checkPrincipal(certs, user) {
+		return errors.New("certificates valid principal differs from username provided")
+	}
+
+	delete(c, fingerprint)
 	return nil
 }
 
@@ -143,6 +181,16 @@ func RevokeHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprintf(w, "%s", err.Error())
 	}
+}
+
+// Fingerprint for a publick key is the md5 sum of the base64 encoded key.
+func getFingerPrint(publicKey string) (fp [16]byte, err error) {
+	data, err := base64.StdEncoding.DecodeString(strings.Split(publicKey, " ")[1])
+	if err != nil {
+		return fp, err
+	}
+	fp = md5.Sum(data)
+	return fp, nil
 }
 
 func main() {
